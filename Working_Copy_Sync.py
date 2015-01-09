0e4eda34ad1d846bd670c8305441cafcaee85773
Steve Mathias <mathiasengineering@gmail.com>
2015-01-08 17:23:33 -0700

    More URL encoding

--- Working_Copy_Sync.py
+++ Working_Copy_Sync.py
@@ -23,26 +23,23 @@ def update_wc(sender):
 @ui.in_background    
 def commit(sender):
     info = os.path.split(editor.get_path())
-    path = info[0].split('/')[-1]
+    repo = info[0].split('/')[-1]
     message = console.input_alert('Commit Message', 'Attach a message to your commit')
-#    encoded_m = message.replace(' ', '%20')
-#    encoded_p = path.replace(' ', '%20')
-#    url = 'working-copy://x-callback-url/commit/?repo=' + path + '&message=' + message + '&x-success=pythonista://'
     url = 'working-copy://x-callback-url/commit/?'
     f = {'repo':repo,'message':message,'x-success':'pythonista://'}
     url += urllib.urlencode(f).replace('+','%20')
-    wb.open(urllib.urlencode(url))
+    print url
+    wb.open(url)
     
 @ui.in_background    
 def commitOne(sender):                                                                                                   
     info = os.path.split(editor.get_path())
-    file_name = info[1]
-    path = info[0].split('/')[-1]
+    repo = info[0].split('/')[-1]
+    path = info[1]
     message = console.input_alert('Commit Message', 'Attach a message to your commit')
-    encoded_m = message.replace(' ', '%20')
-    encoded_p = path.replace(' ', '%20')
-    encoded_f = file_name.replace(' ', '%20')
-    url = 'working-copy://x-callback-url/commit/?repo=' + encoded_p + '&path=' + encoded_f + '&message=' + encoded_m + '&x-success=pythonista://'
+    url = 'working-copy://x-callback-url/commit/?'
+    f = {'repo':repo,'message':message,'x-success':'pythonista://'}
+    url += urllib.urlencode(f).replace('+','%20')
     wb.open(url)
     
 def pushRepo(sender):
